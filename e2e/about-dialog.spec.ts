2f2873ff5be0578382b7a0430a050585be28905b
Andreas Jung <info@zopyx.com>
2026-09-13 16:16:05 +0200

    style(e2e): sort the about dialog spec's imports

--- e2e/about-dialog.spec.ts
+++ e2e/about-dialog.spec.ts
@@ -1,6 +1,5 @@
-import { readFileSync } from "node:fs";
-
 import { expect, test } from "@playwright/test";
+import { readFileSync } from "node:fs";
 
 import { installTauriShim } from "./support/app";
 
